4fe12284b5eb902292e3e2f4f21c40eeddc1773f
Reytler Souza <reytlercompare@hotmail.com>
2024-05-08 17:16:18 -0300

    Alteração da base URL

--- BlazorShop.Web/Program.cs
+++ BlazorShop.Web/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = "https://localhost:7174";
+
+builder.Services.AddScoped(sp => new HttpClient { 
+    BaseAddress = new Uri(baseUrl) 
+});
 
 await builder.Build().RunAsync();
