18e7c491ad293a2f08b72f5bce72eff74b5e58c8
Reytler Souza <reytlercompare@hotmail.com>
2024-05-08 12:48:42 -0300

    Criando controller de produtos

---
+++ BlazorShop.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,81 @@
+﻿using BlazorShop.Api.Mappings;
+using BlazorShop.Api.Repositories;
+using BlazorShop.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutosController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProdutoDto>>> GetItens()
+        {
+            try 
+            { 
+                var produtos = await _produtoRepository.GetItens();
+
+                if(produtos is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    var produtoDtos = produtos.ConverterProdutosParaDto();
+                    return Ok(produtoDtos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProdutoDto>> GetItem(int id)
+        {
+            try
+            {
+                var produto = await _produtoRepository.GetItem(id);
+
+                if (produto is null)
+                {
+                    return NotFound("Produto não localizado");
+                }
+                else
+                {
+                    var produtoDto = produto.ConverterProdutoParaDto();
+                    return Ok(produtoDto);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetItensPorCategoria/{categoriaId}")]
+        public async Task<ActionResult<IEnumerable<ProdutoDto>>> GetItensPorCategoria(int categoriaId)
+        {
+            try
+            {
+                var produtos = await _produtoRepository.GetItensPorCategoria(categoriaId);
+                var produtosDto = produtos.ConverterProdutosParaDto();
+                return Ok(produtosDto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
