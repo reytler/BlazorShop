9b4c9c4cf88af6bb994a8f733b2c0e09ff4f4466
Reytler Souza <reytlercompare@hotmail.com>
2024-05-08 10:44:02 -0300

    Criação dos DTOs

---
+++ BlazorShop.Models/DTOs/CarrinhoItemAtualizaQuantidade.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorShop.Models.DTOs
+{
+    internal class CarrinhoItemAtualizaQuantidade
+    {
+        public int CarrinhoItemId { get; set; }
+        public int Quantidade { get; set;}
+    }
+}
