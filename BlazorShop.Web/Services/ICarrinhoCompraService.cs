964180bfe6b583b160b323d744012346b5e92190
Reytler Souza <reytlercompare@hotmail.com>
2024-05-14 16:02:46 -0300

    Adicionando serviço do carrinho de compras

---
+++ BlazorShop.Web/Services/ICarrinhoCompraService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.Web.Services
+{
+    public interface ICarrinhoCompraService
+    {
+        Task<List<CarrinhoItemDto>> GetItens(string usuarioId);
+        Task<CarrinhoItemDto> AdicionaItem(CarrinhoItemAdicionaDto carrinhoItemAdicionaDto);
+    }
+}
---
+++ BlazorShop.Web/Services/Implementations/CarrinhoCompraService.cs
@@ -0,0 +1,69 @@
+﻿using BlazorShop.Models.DTOs;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace BlazorShop.Web.Services.Implementations
+{
+    public class CarrinhoCompraService : ICarrinhoCompraService
+    {
+        private readonly HttpClient httpClient;
+
+        public CarrinhoCompraService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<CarrinhoItemDto> AdicionaItem(CarrinhoItemAdicionaDto carrinhoItemAdicionaDto)
+        {
+            try
+            {
+                var response = await httpClient.
+                        PostAsJsonAsync<CarrinhoItemAdicionaDto>("api/CarrinhoCompra", carrinhoItemAdicionaDto);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == HttpStatusCode.NoContent) { 
+                        return default(CarrinhoItemDto);
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<CarrinhoItemDto>();
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"{response.StatusCode} Message -{message}");
+                }
+            }
+            catch (Exception) {
+                throw;
+            }    
+        }
+
+        public async Task<List<CarrinhoItemDto>> GetItens(string usuarioId)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync($"api/CarrinhoCompra/{usuarioId}/GetItens");
+
+                if(response.IsSuccessStatusCode)
+                {
+                    if(response.StatusCode == HttpStatusCode.NoContent)
+                    {
+                        return Enumerable.Empty<CarrinhoItemDto>().ToList();
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<List<CarrinhoItemDto>>();
+                }
+                else 
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Http Status Code: {response.StatusCode} Mensagem: {message}");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
