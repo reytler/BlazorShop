ff00f64787ef1237755085f7095ffda89c3a052e
Reytler Souza <reytlercompare@hotmail.com>
2024-05-14 16:48:47 -0300

    Adicionando o serviço CarrinhoCompra no componente ProdutoDetalhes

---
+++ BlazorShop.Web/UsuarioLogado.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShop.Web
+{
+    //Classe com dados fixos pois o serviço de autenticação ainda não foi implementado
+    public class UsuarioLogado
+    {
+        public const string UsuarioId = "1";
+        public const int CarrinhoId = 1;
+    }
+}
