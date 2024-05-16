3124a64a6683afd7f5ffeaf12905afe73fb35ae5
Reytler Souza <reytlercompare@hotmail.com>
2024-05-16 13:19:36 -0300

    Adicionando função AtualizaQuantidade ao serviço CarrinhoCompraService

--- BlazorShop.Web/Services/Implementations/CarrinhoCompraService.cs
+++ BlazorShop.Web/Services/Implementations/CarrinhoCompraService.cs
@@ -1,6 +1,8 @@
 ﻿using BlazorShop.Models.DTOs;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 
 namespace BlazorShop.Web.Services.Implementations
 {
@@ -37,6 +39,26 @@ namespace BlazorShop.Web.Services.Implementations
             catch (Exception) {
                 throw;
             }    
+        }
+
+        public async Task<CarrinhoItemDto> AtualizaQuantidade(CarrinhoItemAtualizaQuantidadeDto 
+            carrinhoItemAtualizaQuantidadeDto)
+        {
+            try
+            {
+                var jsonRequest = JsonSerializer.Serialize(carrinhoItemAtualizaQuantidadeDto);
+                var content = new StringContent(jsonRequest, Encoding.UTF8,"application/json-patch+json");
+                var response = await httpClient.PatchAsync(
+                    $"api/CarrinhoCompra/{carrinhoItemAtualizaQuantidadeDto.CarrinhoItemId}",content);
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<CarrinhoItemDto>();
+                }
+                return null;
+            }catch (Exception)
+            {
+                throw;
+            }
         }
 
         public async Task<CarrinhoItemDto> DeletaItem(int id)
